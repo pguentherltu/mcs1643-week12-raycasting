d79a31f8f2b0fd49b367abcbe730e711f9e437e2
Peter Guenther <144723434+pguentherltu@users.noreply.github.com>
2023-11-08 17:11:15 -0500

    added layer checking

--- Assets/Scripts/RaycastingDemo.cs
+++ Assets/Scripts/RaycastingDemo.cs
@@ -7,22 +7,30 @@ public class RaycastingDemo : MonoBehaviour
 {
     public TextMeshProUGUI objectTMP;
     public TextMeshProUGUI distanceTMP;
+    public Vector3 raycastOriginOffset = new Vector3(0, .75f, 0);
 
     private RaycastHit hit;
-    private Vector3 raycastOriginOffset = new Vector3(0, .75f, 0);
+    private LayerMask mask;
 
-    void Update()
+    private void Start()
+    {
+        mask =~ LayerMask.GetMask("Invisible");
+    }
+
+    void FixedUpdate()
     {
         bool result = Physics.Raycast(
             transform.position + raycastOriginOffset,
             transform.forward,
             out hit,
-            20.0f
+            20.0f,
+            mask
         );
 
         if (result)
+            
         {
-            objectTMP.text = hit.collider.transform.name;
+            objectTMP.text = hit.transform.name;
             distanceTMP.text = hit.distance.ToString();  
         }
         else
